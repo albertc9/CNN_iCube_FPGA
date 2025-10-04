4563eb2f94e4a28256e97a4afddc6dc80d533d61
albert_fedora <albert.chg9@gmail.com>
2025-10-04 15:30:25 -0700

    updated board

--- hls_cnn_conv2d/build_lib.sh
+++ hls_cnn_conv2d/build_lib.sh
@@ -9,7 +9,7 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=BcF8DDCE
+LIB_STAMP=DadBab08
 
 ${CC} ${CFLAGS} ${INCFLAGS} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
 ${CC} ${CFLAGS} ${INCFLAGS} -c ${PROJECT}_bridge.cpp -o ${PROJECT}_bridge.o
